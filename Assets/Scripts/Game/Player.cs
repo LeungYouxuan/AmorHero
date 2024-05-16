8b463e730e1fb4561454a6a3d2f389c3c06b1771
Splendid <1831560104@qq.com>
2024-05-16 22:01:11 +0800

    新增人物待机，跑步，下蹲的状态

--- Assets/Scripts/Game/Player.cs
+++ Assets/Scripts/Game/Player.cs
@@ -10,24 +10,24 @@ namespace QFramework.AmorHero
 	{
 		protected override void OnExecute()
 		{
-			float duration=Time.time-this.GetModel<PlayerModel>().currentTime;//跑步的那些帧数所经过的时间
-			Debug.Log("帧消耗时间:"+duration);
+			float duration = Time.time - this.GetModel<PlayerModel>().currentTime;//跑步的那些帧数所经过的时间
+			Debug.Log("帧消耗时间:" + duration);
 			this.GetModel<PlayerModel>().playerRunTime += duration;
 			this.SendEvent<PlayerRunTimeChangeEvent>();
 		}
 	}
-    public class InCreaseGameTime : AbstractCommand
-    {
-        protected override void OnExecute()
-        {
-           float duration=Time.time-this.GetModel<PlayerModel>().currentTime;
-		   this.GetModel<PlayerModel>().currentTime+=duration;
-		   this.SendEvent<GameTimeChangeEvent>();
-        }
-    }
-    #endregion
-    #region Event
-    public struct PlayerRunTimeChangeEvent
+	public class InCreaseGameTime : AbstractCommand
+	{
+		protected override void OnExecute()
+		{
+			float duration = Time.time - this.GetModel<PlayerModel>().currentTime;
+			this.GetModel<PlayerModel>().currentTime += duration;
+			this.SendEvent<GameTimeChangeEvent>();
+		}
+	}
+	#endregion
+	#region Event
+	public struct PlayerRunTimeChangeEvent
 	{
 
 	}
@@ -43,7 +43,7 @@ namespace QFramework.AmorHero
 		protected override void OnInit()
 		{
 			playerRunTime = 0;
-			currentTime=Time.time;
+			currentTime = Time.time;
 		}
 	}
 	public class PlayerApp : Architecture<PlayerApp>
@@ -57,14 +57,14 @@ namespace QFramework.AmorHero
 	{
 		public enum PlayerState
 		{
-			待机, 跑步,
+			待机, 跑步, 下蹲, 跳跃
 		}
 		public FSM<PlayerState> playerFSM = new FSM<PlayerState>();
 		public Vector2 currentDir = new Vector2(1, 0);//主角默认朝向
 		public float runSpeed;//玩家基础移动速度大小
 		public Animator playerAnimator;//玩家动画控制器组件
-		private PlayerModel playerModel;
-		Rigidbody2D mRigidBody;
+		public PlayerModel playerModel;
+		public Rigidbody2D mRigidBody;
 		void Start()
 		{
 			// Code Here
@@ -72,80 +72,18 @@ namespace QFramework.AmorHero
 			mRigidBody = GetComponent<Rigidbody2D>();
 			playerAnimator = GetComponent<Animator>();
 			#region 状态
-			playerFSM.State(PlayerState.待机)
-			.OnCondition(() => true)
-			.OnEnter(() =>
-			{
-				Debug.Log("进入待机模式");
-				gameObject.transform.localScale = new Vector3(currentDir.x, 1, 0);
-				playerAnimator.SetFloat("runSpeed", 0f);
-				mRigidBody.velocity = Vector2.zero;
-			})
-			.OnUpdate(() =>
-			{
-				//监听玩家控制
-				float runX = Input.GetAxisRaw("Horizontal");
-				float runY = Input.GetAxisRaw("Vertical");
-				gameObject.transform.localScale = new Vector3(currentDir.x, 1, 1);
-				//只有横向移动，说明玩家是跑步
-				if (runX != 0 && runY == 0)
-				{
-					playerFSM.ChangeState(PlayerState.跑步);
-				}
-			})
-			.OnExit(() =>
-			{
-				Debug.Log("退出待机模式");
-			})
-			;
-			playerFSM.State(PlayerState.跑步)
-			.OnCondition(() => true)
-			.OnEnter(() =>
-			{
-				Debug.Log("进入跑步状态");
-			})
-			.OnUpdate(() =>
-			{
-				this.SendCommand<IncreaseRunTime>();
-			})
-			.OnFixedUpdate(() =>
-			{
-
-				float runX = Input.GetAxisRaw("Horizontal");
-				float runY = Input.GetAxisRaw("Vertical");
-				//横向方向没速度说明玩家不处于跑步
-				if (runX == 0)
-				{
-					mRigidBody.velocity = Vector2.zero;
-					playerFSM.ChangeState(PlayerState.待机);
-				}
-				else
-					Run(runX, runY);
-			})
-			.OnExit(() =>
-			{
-				Debug.Log("退出跑步状态");
-			});
+			playerFSM.AddState(PlayerState.待机, new PlayerIdleState(playerFSM, this));
+			playerFSM.AddState(PlayerState.跑步, new PlayerRunState(playerFSM, this));
+			playerFSM.AddState(PlayerState.下蹲, new PlayerCrouchState(playerFSM, this));
 			#endregion
 			playerFSM.StartState(PlayerState.待机);
 			this.RegisterEvent<PlayerRunTimeChangeEvent>(e =>
 			{
 				Debug.Log("跑步时长：" + playerModel.playerRunTime);
 			}).UnRegisterWhenGameObjectDestroyed(gameObject);
-			this.RegisterEvent<GameTimeChangeEvent>(e=>
+			this.RegisterEvent<GameTimeChangeEvent>(e =>
 			{
-				if(Mathf.Approximately(playerModel.currentTime,10f))
-				{
-					Debug.Log("到达10秒!");
-				}
-				else if(playerModel.currentTime>10f)
-				{
 
-				}
-				else
-				{
-					Debug.Log("游戏运行时间："+playerModel.currentTime);
-				}
 			}).UnRegisterWhenGameObjectDestroyed(gameObject);
 		}
 		void Update()
@@ -161,7 +99,7 @@ namespace QFramework.AmorHero
 		{
 			playerFSM.Clear();
 		}
-		void Run(float runX, float runY)
+		public void Run(float runX, float runY)
 		{
 			gameObject.transform.localScale = new Vector3(runX, 1, 0);
 			mRigidBody.velocity = new Vector2(runX, runY) * runSpeed;
@@ -172,6 +110,123 @@ namespace QFramework.AmorHero
 		public IArchitecture GetArchitecture()
 		{
 			return PlayerApp.Interface;
+		}
+	}
+	public class PlayerIdleState : AbstractState<Player.PlayerState, Player>
+	{
+		public PlayerIdleState(FSM<Player.PlayerState> fsm, Player target) : base(fsm, target)
+		{
+		}
+		protected override bool OnCondition()
+		{
+			return base.OnCondition();
+		}
+		protected override void OnEnter()
+		{
+			base.OnEnter();
+			Debug.Log("进入待机模式");
+			mTarget.gameObject.transform.localScale = new Vector3(mTarget.currentDir.x, 1, 0);
+			mTarget.playerAnimator.SetFloat("runSpeed", 0f);
+			mTarget.mRigidBody.velocity = Vector2.zero;
+		}
+		protected override void OnUpdate()
+		{
+			base.OnUpdate();
+			//监听玩家控制
+			float runX = Input.GetAxisRaw("Horizontal");
+			float runY = Input.GetAxisRaw("Vertical");
+			mTarget.gameObject.transform.localScale = new Vector3(mTarget.currentDir.x, 1, 1);
+			//只有横向移动，说明玩家是跑步
+			if (runX != 0 && runY == 0)
+			{
+				mTarget.playerFSM.ChangeState(Player.PlayerState.跑步);
+			}
+			if (Input.GetKeyDown(KeyCode.S))
+			{
+				mTarget.playerFSM.ChangeState(Player.PlayerState.下蹲);
+			}
+		}
+		protected override void OnFixedUpdate()
+		{
+			base.OnFixedUpdate();
+		}
+		protected override void OnExit()
+		{
+			base.OnExit();
+			Debug.Log("退出待机模式");
+		}
+	}
+	public class PlayerRunState : AbstractState<Player.PlayerState, Player>
+	{
+		public PlayerRunState(FSM<Player.PlayerState> fsm, Player target) : base(fsm, target)
+		{
+
+		}
+		protected override bool OnCondition()
+		{
+			return base.OnCondition();
+		}
+		protected override void OnEnter()
+		{
+			base.OnEnter();
+			Debug.Log("进入跑步状态");
+		}
+		protected override void OnUpdate()
+		{
+			base.OnUpdate();
+			if (Input.GetKeyDown(KeyCode.S))
+			{
+				mTarget.playerFSM.ChangeState(Player.PlayerState.下蹲);
+			}
+			else
+				mTarget.SendCommand<IncreaseRunTime>();
+		}
+		protected override void OnFixedUpdate()
+		{
+			base.OnFixedUpdate();
+			float runX = Input.GetAxisRaw("Horizontal");
+			float runY = Input.GetAxisRaw("Vertical");
+			//横向方向没速度说明玩家不处于跑步
+			if (runX == 0)
+			{
+				mTarget.mRigidBody.velocity = Vector2.zero;
+				mTarget.playerFSM.ChangeState(Player.PlayerState.待机);
+			}
+			else
+				mTarget.Run(runX, runY);
+		}
+		protected override void OnExit()
+		{
+			base.OnExit();
+			Debug.Log("退出跑步状态");
+		}
+	}
+	public class PlayerCrouchState : AbstractState<Player.PlayerState, Player>
+	{
+		public PlayerCrouchState(FSM<Player.PlayerState> fsm, Player target) : base(fsm, target)
+		{
+		}
+		protected override void OnEnter()
+		{
+			base.OnEnter();
+			mTarget.playerAnimator.SetBool("isCrouch", true);
+		}
+		protected override void OnUpdate()
+		{
+			if (Input.GetKeyUp(KeyCode.S))
+			{
+				mFSM.ChangeState(Player.PlayerState.待机);
+			}
+		}
+		protected override void OnFixedUpdate()
+		{
+			base.OnFixedUpdate();
+			mTarget.mRigidBody.velocity = Vector2.zero;
+		}
+		protected override void OnExit()
+		{
+			base.OnExit();
+			mTarget.playerAnimator.SetBool("isCrouch", false);
 		}
 	}
 }
